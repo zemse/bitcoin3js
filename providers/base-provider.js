2da9b9692a92669043737b1435bf77a4807fe307
zemse <sohamzemse143@gmail.com>
2020-04-11 12:36:52 +0530

    add block pooling

--- providers/base-provider.js
+++ providers/base-provider.js
@@ -4,11 +4,25 @@ class BaseProvider {
   /// @dev engine is the provider engine object
   constructor(engine) {
     this._engine = engine;
+    this._lastBlockNumber = -1;
+    this._promises = [];
+
+    this._promises.push(this.getBlockHeight());
+    this._promises[0].then(value => {
+      this._lastBlockNumber = value;
+    });
+
+    this._pollingInterval = 30000;
+    this._events = [];
   }
 
   async getBlockHeight() {
     const response = await axios.get(this._engine.functions.blockHeight.url());
-    return this._engine.functions.blockHeight.parse(response.data);
+    const newBlockHeight = this._engine.functions.blockHeight.parse(response.data);
+    if(newBlockHeight > this._lastBlockNumber) {
+      this._lastBlockNumber = newBlockHeight;
+    }
+    return newBlockHeight;
   }
 
   async getBalance(address) {
@@ -19,6 +33,42 @@ class BaseProvider {
   async getUtxos(address) {
     const response = await axios.get(this._engine.functions.utxos.url(address));
     return this._engine.functions.utxos.parse(response.data);
+  }
+
+  on(event, callback) {
+    switch(event) {
+      case 'block':
+        this.onBlock(callback);
+        break;
+      default:
+        throw new Error(`event type '${event}' is not supported`);
+        break;
+    }
+  }
+
+  onBlock(callback) {
+    const intervalId = setInterval(async() => {
+      const newBlockHeight = await this.getBlockHeight();
+      if(newBlockHeight > this._lastBlockNumber) {
+        this._lastBlockNumber = newBlockHeight;
+        callback(newBlockHeight);
+      }
+    }, this._pollingInterval);
+
+    this._events.push({
+      type: 'new-block',
+      intervalId,
+      stopEvent: this.stopEvent.bind(this, this._events.length)
+    });
+  }
+
+  stopEvent(index) {
+    if(typeof index === 'number' && index < this._events.length) {
+      clearInterval(this._events[index].intervalId);
+      this._events.splice(index,1);
+    } else {
+      throw new Error('Invalid event index');
+    }
   }
 }
 
--- test/providers.test.js
+++ test/providers.test.js
@@ -13,14 +13,21 @@ describe('Blockcypher Provider', () => {
 
   it('should resolve current block height', async() => {
     const provider = new bitcoin.providers.BlockcypherProvider('test3');
-
-    console.log({provider});
     const height = await provider.getBlockHeight();
-
-    console.log({provider});
 
     assert.equal(typeof height, 'number', 'should be a number');
     assert.ok(!isNaN(height), 'should not be NaN');
     assert.ok(height > 1697056, 'height should be correct');
+  });
+
+  it('onblock initiate and stop', async() => {
+    const provider = new bitcoin.providers.BlockcypherProvider('test3');
+    provider.on('block', console.log);
+    assert(provider._events.length === 1, 'active event should be added');
+    assert(provider._events[0].type === 'new-block', 'on-block event should be mentioned');
+
+    provider._events[0].stopEvent();
+
+    assert(provider._events.length === 0, 'event should be stopped');
   });
 });
